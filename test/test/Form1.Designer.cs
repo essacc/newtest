ebf7e18e4cd8dc8f23f30200670f9c695c3e0420
essaci <1252378435@qq.com>
2020-10-15 08:44:35 +0800

    初始化仓库
    初始化仓库

---
+++ test/test/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(212, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(654, 319);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(167, 42);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(386, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 343);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ test/test/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TwinCAT.Ads;
+using System.Xml;
+using TCatSysManagerLib;
+using System.Net;
+using FluentFTP;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        bool progres = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "ftp://" + "192.168.1.12" + "/TwinCAT/3.1/";
+            deletboot(path);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string basePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            if (Directory.Exists(basePath))
+            {
+                MessageBox.Show("根目录存在");
+            }
+            else
+            {
+                //DirectoryInfo directoryInfo = new DirectoryInfo(basePath);
+                //directoryInfo.Create();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FtpClient conn = new FtpClient();
+            conn.Host = "ftp://" + "192.168.1.12";
+            conn.AutoConnect();
+
+        }
+        private void downloadallfiles(string ftppath,string savepath)
+        {
+            
+        }
+
+        public static string[] GetFolderAndFileList(string pathname, string FtpPath)
+        {
+            string[] getfolderandfilelist;
+            FtpWebRequest request;
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                string uri = FtpPath + "/" + pathname;
+                request = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                request.UseBinary = true;
+                request.Method = WebRequestMethods.Ftp.ListDirectory;
+                request.UseBinary = true;
+                WebResponse response = request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    sb.Append(line);
+                    sb.Append("\n");
+                    line = reader.ReadLine();
+                }
+                sb.Remove(sb.ToString().LastIndexOf('\n'), 1);
+                reader.Close();
+                response.Close();
+                return sb.ToString().Split('\n');
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("获取FTP上面的文件夹和文件：" + ex.Message);
+                getfolderandfilelist = null;
+                return getfolderandfilelist;
+            }
+        }
+        private void deletboot(string path)
+        {
+            string basePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            string localpath = basePath + "/data/Boot";
+            if (Directory.Exists(localpath))
+            {
+
+            }
+            else
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(localpath);
+                directoryInfo.Create();
+            }
+            string[] n1 = GetFolderAndFileList("Bootold", path);
+            if (n1 != null)
+            {
+                foreach (string name1 in n1)
+                {
+                    if(name1.Contains("."))
+                    {
+                        FileDownLoad(name1, path, localpath);
+                    }
+                    else
+                    {
+                        if (Directory.Exists(localpath + "/" + name1+"/"))
+                        {
+
+                        }
+                        else
+                        {
+                            DirectoryInfo directoryInfo = new DirectoryInfo(localpath + "/" + name1+"/");
+                            directoryInfo.Create();
+                        }
+                        string[] n2 = GetFolderAndFileList(name1, path + "/Bootold/");
+                        if(n2!=null)
+                        {
+                            foreach(string name2 in n2)
+                            {
+                                if(name2.Contains("."))
+                                {
+                                    FileDownLoad(name2, path + "/" + name1 + "/", localpath + "/" + name1 + "/");
+                                }
+                                else
+                                {
+                                    if (Directory.Exists(localpath + "/" + name1 + "/"+name2+"/"))
+                                    {
+
+                                    }
+                                    else
+                                    {
+                                        DirectoryInfo directoryInfo = new DirectoryInfo(localpath + "/" + name1 + "/"+name2+"/");
+                                        directoryInfo.Create();
+                                    }
+                                    string[] n3 = GetFolderAndFileList(name2, path + "/Bootold/" + name1 + "/");
+                                    if(n3!=null)
+                                    {
+                                        foreach (string name3 in n3)
+                                        {
+                                            if (name3.Contains("."))
+                                            {
+                                                FileDownLoad(name3, path + "/" + name1 + "/", localpath + "/" + name1 + "/"+name2+"/");
+                                            }
+                                            else
+                                            {
+                                                if (Directory.Exists(localpath + "/" + name1 + "/" + name2 + "/"+name3+"/"))
+                                                {
+
+                                                }
+                                                else
+                                                {
+                                                    DirectoryInfo directoryInfo = new DirectoryInfo(localpath + "/" + name1 + "/" + name2 + "/" + name3 + "/");
+                                                    directoryInfo.Create();
+                                                }
+                                                string[] n4 = GetFolderAndFileList(name3, path + "/Bootold/" + name1 + "/"+name2+"/");
+                                                if (n4 != null)
+                                                {
+                                                    foreach (string name4 in n4)
+                                                    {
+                                                        if (name4.Contains("."))
+                                                        {
+                                                            FileDownLoad(name4, path + "/" + name1 + "/", localpath + "/" + name1 + "/" + name2 + "/" + name3 + "/");
+
+                                                        }
+                                                    }
+                                                }
+
+                                            }
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //if (n1 != null)
+                //{
+                //    foreach (string name1 in n1)
+                //    {
+                //        string path2 = path + "/Bootold/";
+
+                //        if (DeleteFile(name1, path2))
+                //        {
+
+                //        }
+                //        else
+                //        {
+                //            string[] n2 = GetFolderAndFileList(name1, path2);
+                //            if (n2 != null)
+                //            {
+                //                foreach (string name2 in n2)
+                //                {
+
+                //                    if (DeleteFile(name2, path2 + name1))
+                //                    {
+
+                //                    }
+                //                    else
+                //                    {
+                //                        string[] n3 = GetFolderAndFileList(name2, path2 + name1);
+                //                        foreach (string name3 in n3)
+                //                        {
+                //                            if (DeleteFile(name3, path2 + name1 + "/" + name2))
+                //                            {
+
+                //                            }
+                //                            else
+                //                            {
+                //                                string[] n4 = GetFolderAndFileList(name3, path2 + name1 + "/" + name2);
+                //                                if (n4 != null)
+                //                                {
+                //                                    foreach (string name4 in n4)
+                //                                    {
+                //                                        DeleteFile(name4, path2 + name1 + "/" + name2 + "/" + name3);
+                //                                    }
+                //                                }
+                //                            }
+                //                            DeletePath("/" + name3, path2 + name1 + "/" + name2);
+                //                        }
+                //                    }
+                //                    DeletePath("/" + name2, path2 + name1);
+
+                //                }
+
+                //            }
+                //            DeletePath("/" + name1, path2);
+                //        }
+                //        DeletePath("/Bootold/", path);
+                //    }
+                //}
+            }
+
+        public static bool DeleteFile(string fileName, string FtpPath)
+        {
+            try
+            {
+                FileInfo fileInf = new FileInfo(fileName);
+
+                string uri = Path.Combine(FtpPath, fileInf.Name);
+
+                //string uri = FtpPath;
+
+                // 根据uri创建FtpWebRequest对象
+                FtpWebRequest reqFTP = (FtpWebRequest)WebRequest.Create(new Uri(uri));
+
+                // 指定数据传输类型
+                reqFTP.UseBinary = true;
+
+                // ftp用户名和密码                
+
+                // 默认为true，连接不会被关闭
+
+                // 在一个命令之后被执行
+
+                reqFTP.KeepAlive = false;
+
+                // 指定执行什么命令
+
+                reqFTP.Method = WebRequestMethods.Ftp.DeleteFile;
+
+                FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
+
+                response.Close();
+
+                //Successinfo
+
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                //ErrorInfo 
+                return false;
+            }
+
+        }
+
+        public static void DeletePath(string pathname, string FtpPath)
+        {
+            try
+            {
+                string uri = FtpPath + pathname;
+                FtpWebRequest reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                reqFTP.Method = WebRequestMethods.Ftp.RemoveDirectory;
+                FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
+                response.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FTP删除目录：" + ex.Message);
+            }
+        }
+
+        public void FileDownLoad(string fileName,string path, string localpath)        //ftp下载文件
+        {
+            FtpWebRequest request;
+            try
+            {
+                FileStream fs = new FileStream(localpath +"/"+ fileName, FileMode.Create);
+                request = (FtpWebRequest)FtpWebRequest.Create(new Uri(path + "/" + fileName));
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+                request.UseBinary = true;
+                request.UsePassive = false;
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                int bufferLength = 5120;
+                int i;
+                byte[] buffer = new byte[bufferLength];
+                i = stream.Read(buffer, 0, bufferLength);
+                while (i > 0)
+                {
+                    fs.Write(buffer, 0, i);
+                    i = stream.Read(buffer, 0, bufferLength);
+                }
+                stream.Close();
+                fs.Close();
+                response.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("FTP下载文件：" + ex.Message);
+            }
+        }
+
+    }
+}
